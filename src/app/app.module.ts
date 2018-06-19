bdaba63e5ff0d95afb72e4a834c9401e13d31edf
Mani Kumar K V <mkanamarlapudi@nasinsurance.com>
2018-06-19 17:14:39 +0530

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +21,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDAT_PmwWEw8ltk5DPGQ1UGiv5EQlNbE8s",
+      authDomain: "billingapp-ccfaf.firebaseapp.com",
+      databaseURL: "https://billingapp-ccfaf.firebaseio.com/",
+      projectId: "billingapp-ccfaf",
+      storageBucket: "billingapp-ccfaf.appspot.com",
+      messagingSenderId: "417451885087"
+    }),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  billList: AngularFireList<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public afDatabase: AngularFireDatabase
+  ) {
+    this.billList = afDatabase.list("/bills");
   }
-
+  createBill(name, amount, dueDate) {
+    const newBillRef = this.billList.push({});
+    console.log("11", this.billList);
+    newBillRef
+      .set({
+        name: name,
+        amount: amount,
+        dueDate: dueDate,
+        paid: false,
+        id: newBillRef.key
+      })
+      .then(
+        newBill => {
+          //this.navCtrl.pop();
+          console.log("22", newBill);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
 }
