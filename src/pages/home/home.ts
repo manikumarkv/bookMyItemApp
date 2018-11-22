2f4f6c73cd2a822a820976f70c7e5b723ce51b21
Mani Kumar K V <mkanamarlapudi@nasinsurance.com>
2018-11-22 21:43:16 +0530

    removed angular fire

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,40 +1,17 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
-import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
-  billList: AngularFireList<any>;
+  billList: any[];
 
-  constructor(
-    public navCtrl: NavController,
-    public afDatabase: AngularFireDatabase,    
-  ) {
-    this.billList = afDatabase.list("/bills");
+  constructor(public navCtrl: NavController) {
+    this.billList = [];
   }
 
   createBill(name, amount, dueDate) {
-    const newBillRef = this.billList.push({});
-    console.log("11", this.billList);
-    newBillRef
-      .set({
-        name: name,
-        amount: amount,
-        dueDate: dueDate,
-        paid: false,
-        id: newBillRef.key
-      })
-      .then(
-        newBill => {
-          //this.navCtrl.pop();
-          console.log("22", newBill);
-        },
-        error => {
-          console.log(error);
-        }
-      );
+    const newBillRef = [];
   }
-  
 }
